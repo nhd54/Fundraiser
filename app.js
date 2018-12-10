a4cf1754fc5d2d4c421d83fb60d2cae9d49cd86c
nhd54 <nicklasdegnebolig@gmail.com>
2018-12-10 10:25:51 +0100

    donation and map

--- app.js
+++ app.js
@@ -19,57 +19,24 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const logonButton = document.querySelector("#login");
 
-let fyn = [];
-let jylland = [];
-let sjælland = [];
-let donations = [];
+function login() {
+  logonButton.addEventListener("click", () => {
+    db.collection("users")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          if (
+            username.value == doc.data().username &&
+            password.value == doc.data().password
+          ) {
+            window.location.href = "dashboard.html";
+            //console.log("vi er inde");
 
-logonButton.addEventListener("click", () => {
-  db.collection("users")
-    .get()
-    .then(snapshot => {
-      snapshot.docs.forEach(doc => {
-        if (
-          username.value == doc.data().username &&
-          password.value == doc.data().password
-        ) {
-          window.location.href = "dashboard.html";
-          //console.log("vi er inde");
-
-          return;
-        } else {
-          console.log("incorrect username or password");
-        }
+            return;
+          } else {
+            console.log("incorrect username or password");
+          }
+        });
       });
-    });
-});
-
-db.collection("donations")
-  .get()
-  .then(snapshot => {
-    snapshot.docs.forEach(doc => {
-      donations.push(doc.data());
-    });
   });
-
-function test() {
-  donations.forEach(country => {
-    if (country.country == "fyn") {
-      fyn.push(country);
-    } else if (country.country == "sjælland") {
-      sjælland.push(country);
-    } else if (country.country == "jylland") {
-      jylland.push(country);
-    }
-  });
-}
-
-function count() {
-  if (jylland.lenght > sjælland.lengt && jylland.lengt > fyn.length) {
-    console.log("vis jylland");
-  } else if (sjælland.lenght > jylland.lengt && sjælland.lengt > fyn.length) {
-    console.log("vis sjælland");
-  } else {
-    console.log("vis fyn");
-  }
 }
